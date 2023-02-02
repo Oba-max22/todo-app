232e706491bdc053a99b0b0ebfb094a445a694be
EMMANUEL MACAULAY <emmanuelmcl86@gmail.com>
2023-02-02 18:09:33 +0100

    modified TodoModel

--- models/TodoModel.js
+++ models/TodoModel.js
@@ -6,19 +6,6 @@ const todoSchema = new Schema({
     title: {
         type: String,
         required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    status: {
-        type: String,
-        required: true,
-        enum: ['todo', 'in-progress', 'done']
-    },
-    dueDate: {
-        type: Date,
-        required: true
     }
 });
 
